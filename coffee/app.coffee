cdf2e06b684c5ab72925ec8c3a79ba476d167817
cav_dan <dcc69eyes@gmail.com>
2013-05-29 10:19:27 -0300

    Objetos e fluxo
    TODOs: cdp começa por 1, img/conteudo... , achar um evento razoável,
    criar cores etc

--- coffee/app.coffee
+++ coffee/app.coffee
@@ -1,4 +1,39 @@
-produtos = $.getJSON('json/produtos.json', -> 
-	console.log($.parseJSON(produtos.responseText))
+fetch = (arquivo) -> 
+	listaDeObj = new Array
+	$.getJSON 'json/'+ arquivo, (arq) ->
+
+		tipoAnterior = new String;
+		cdp = 0;
+		
+		$.each(arq, (k, v) ->
+			if v.select is tipoAnterior then cdp += 1 else cdp = 0
+			tipoAnterior = v.select
+
+			p = new produto parseInt(cdp), v.nome, v.select, v.cores			
+			listaDeObj.push(p)
+		) 
 	
-	)
+	return listaDeObj
+
+
+class produto
+	constructor : (@cdp, @nome, @tipo, @cores) ->
+		@select = $('#' + @tipo)
+		@el = '<option value="' + @tipo + @cdp + '">' + @nome + '</option> '
+		
+		$(@select).append(@el)
+
+		$(@select).on('blur', ->
+		$('#canvas .' + @tipo + ' img').attr('src', ->
+			dir = 'img/' + tipo + '/' + cdp + '.png';)
+		)
+
+criarProdutos = ->
+		lista = fetch 'produtos.json'
+		console.log lista
+		console.log 'wateva'
+
+
+criarProdutos()
+
+console.log $('#malha').prop('selectedIndex');
--- js/app.js
+++ js/app.js
@@ -1,8 +1,61 @@
 (function() {
-  var produtos;
+  var criarProdutos, fetch, produto;
 
-  produtos = $.getJSON('json/produtos.json', function() {
-    return console.log($.parseJSON(produtos.responseText));
-  });
+  fetch = function(arquivo) {
+    var listaDeObj;
+
+    listaDeObj = new Array;
+    $.getJSON('json/' + arquivo, function(arq) {
+      var cdp, tipoAnterior;
+
+      tipoAnterior = new String;
+      cdp = 0;
+      return $.each(arq, function(k, v) {
+        var p;
+
+        if (v.select === tipoAnterior) {
+          cdp += 1;
+        } else {
+          cdp = 0;
+        }
+        tipoAnterior = v.select;
+        p = new produto(parseInt(cdp), v.nome, v.select, v.cores);
+        return listaDeObj.push(p);
+      });
+    });
+    return listaDeObj;
+  };
+
+  produto = (function() {
+    function produto(cdp, nome, tipo, cores) {
+      this.cdp = cdp;
+      this.nome = nome;
+      this.tipo = tipo;
+      this.cores = cores;
+      this.select = $('#' + this.tipo);
+      this.el = '<option value="' + this.tipo + this.cdp + '">' + this.nome + '</option> ';
+      $(this.select).append(this.el);
+      $(this.select).on('blur', function() {}, $('#canvas .' + this.tipo + ' img').attr('src', function() {
+        var dir;
+
+        return dir = 'img/' + tipo + '/' + cdp + '.png';
+      }));
+    }
+
+    return produto;
+
+  })();
+
+  criarProdutos = function() {
+    var lista;
+
+    lista = fetch('produtos.json');
+    console.log(lista);
+    return console.log('wateva');
+  };
+
+  criarProdutos();
+
+  console.log($('#malha').prop('selectedIndex'));
 
 }).call(this);
