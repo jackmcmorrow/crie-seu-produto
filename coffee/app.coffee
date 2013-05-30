e7d769e2813b22926c99125983d1d49288e133d7
cav_dan <dcc69eyes@gmail.com>
2013-05-30 03:48:06 -0300

    build funcionando!
    Agora que já seleciona, bota cor e coisas assim vem a hora de otimizar e
    ver qual é a do jotformz, talvez eu tenha que mudar bastante coisa

--- coffee/app.coffee
+++ coffee/app.coffee
@@ -3,13 +3,13 @@ fetch = (arquivo) ->
 	$.getJSON 'json/'+ arquivo, (arq) ->
 
 		tipoAnterior = new String;
-		cdp = 0;
+		cdp = 1;
 		
 		$.each(arq, (k, v) ->
-			if v.select is tipoAnterior then cdp += 1 else cdp = 0
+			if v.select is tipoAnterior then cdp += 1 else cdp = 1
 			tipoAnterior = v.select
 
-			p = new produto parseInt(cdp), v.nome, v.select, v.cores			
+			p = new produto parseInt(cdp), v.nome, v.tipo, v.cores			
 			listaDeObj.push(p)
 		) 
 	
@@ -19,19 +19,40 @@ fetch = (arquivo) ->
 class produto
 	constructor : (@cdp, @nome, @tipo, @cores) ->
 		@select = $('#' + @tipo)
-		@el = '<option value="' + @tipo + @cdp + '">' + @nome + '</option> '
+		@oid = @tipo + @cdp
+		@el = '<option value="' + @oid + '" nome="' + @nome + '">' + @nome + '</option>'
 		
 		$(@select).append(@el)
 
-		$(@select).on('blur', ->
-		$('#canvas .' + @tipo + ' img').attr('src', ->
-			dir = 'img/' + tipo + '/' + cdp + '.png';)
-		)
+		
 
 criarProdutos = ->
 		lista = fetch 'produtos.json'
-		console.log lista
-		console.log 'wateva'
+		tipos = ['manequim', 'malha', 'cor', 'gola', 'punho'];
+
+		corCdp = 0
+		criarCores = (oid)->
+			$('#cor option').remove()
+			
+			criarCor = (cores)->
+				c = new produto corCdp += 1, j.cor, 'cor' for j in cores
+
+			criarCor i.cores for i in lista when i.tipo + i.cdp is oid
+			listenEvent 'cor'
+
+
+		listenEvent = (tipo) ->
+			$('select#' + tipo).on('change', ->
+				oid = $(@).val();
+				$('#canvas .' + tipo + ' img').attr('src', -> 
+					dir = 'img/' + tipo + '/' + oid + '.png'
+				)
+
+				criarCores oid if tipo is 'malha'
+			)
+
+		listenEvent tipo for tipo in tipos
+			
 
 
 criarProdutos()
--- js/app.js
+++ js/app.js
@@ -9,17 +9,17 @@
       var cdp, tipoAnterior;
 
       tipoAnterior = new String;
-      cdp = 0;
+      cdp = 1;
       return $.each(arq, function(k, v) {
         var p;
 
         if (v.select === tipoAnterior) {
           cdp += 1;
         } else {
-          cdp = 0;
+          cdp = 1;
         }
         tipoAnterior = v.select;
-        p = new produto(parseInt(cdp), v.nome, v.select, v.cores);
+        p = new produto(parseInt(cdp), v.nome, v.tipo, v.cores);
         return listaDeObj.push(p);
       });
     });
@@ -33,13 +33,9 @@
       this.tipo = tipo;
       this.cores = cores;
       this.select = $('#' + this.tipo);
-      this.el = '<option value="' + this.tipo + this.cdp + '">' + this.nome + '</option> ';
+      this.oid = this.tipo + this.cdp;
+      this.el = '<option value="' + this.oid + '" nome="' + this.nome + '">' + this.nome + '</option>';
       $(this.select).append(this.el);
-      $(this.select).on('blur', function() {}, $('#canvas .' + this.tipo + ' img').attr('src', function() {
-        var dir;
-
-        return dir = 'img/' + tipo + '/' + cdp + '.png';
-      }));
     }
 
     return produto;
@@ -47,11 +43,54 @@
   })();
 
   criarProdutos = function() {
-    var lista;
+    var corCdp, criarCores, lista, listenEvent, tipo, tipos, _i, _len, _results;
 
     lista = fetch('produtos.json');
-    console.log(lista);
-    return console.log('wateva');
+    tipos = ['manequim', 'malha', 'cor', 'gola', 'punho'];
+    corCdp = 0;
+    criarCores = function(oid) {
+      var criarCor, i, _i, _len;
+
+      $('#cor option').remove();
+      criarCor = function(cores) {
+        var c, j, _i, _len, _results;
+
+        _results = [];
+        for (_i = 0, _len = cores.length; _i < _len; _i++) {
+          j = cores[_i];
+          _results.push(c = new produto(corCdp += 1, j.cor, 'cor'));
+        }
+        return _results;
+      };
+      for (_i = 0, _len = lista.length; _i < _len; _i++) {
+        i = lista[_i];
+        if (i.tipo + i.cdp === oid) {
+          criarCor(i.cores);
+        }
+      }
+      return listenEvent('cor');
+    };
+    listenEvent = function(tipo) {
+      return $('select#' + tipo).on('change', function() {
+        var oid;
+
+        oid = $(this).val();
+        $('#canvas .' + tipo + ' img').attr('src', function() {
+          var dir;
+
+          return dir = 'img/' + tipo + '/' + oid + '.png';
+        });
+        if (tipo === 'malha') {
+          return criarCores(oid);
+        }
+      });
+    };
+    _results = [];
+    for (_i = 0, _len = tipos.length; _i < _len; _i++) {
+      tipo = tipos[_i];
+      _results.push(listenEvent(tipo));
+    }
+    return _results;
   };
 
   criarProdutos();
